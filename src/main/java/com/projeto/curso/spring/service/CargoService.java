6f09081d6b6a48b04c1809f715a5982fc85b2680
Leandro Lima <leandroqlima@gmail.com>
2022-01-19 10:03:59 -0300

    adicionando services

---
+++ src/main/java/com/projeto/curso/spring/service/CargoService.java
@@ -0,0 +1,19 @@
+package com.projeto.curso.spring.service;
+
+import java.util.List;
+
+import com.projeto.curso.spring.domain.Cargo;
+
+public interface CargoService {
+	
+	void salvar(Cargo cargo);
+	
+	void editar(Cargo cargo);
+	
+	void excluir(Long id);
+	
+	Cargo buscarPorId(Long id);
+	
+	List<Cargo> buscarTodos();
+	
+}
